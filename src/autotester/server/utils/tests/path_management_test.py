8fca90da2afe82ca459cab5897cf51455205d5d9
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-03-05 09:13:05 -0500

    test: Added tests for path_management

---
+++ src/autotester/server/utils/tests/path_management_test.py
@@ -0,0 +1,23 @@
+from src.autotester.server.utils.path_management import *
+import tempfile
+import os
+import sys
+
+
+def test_current_directory():
+    tmp_dir = tempfile.gettempdir()
+    curr_dir = os.getcwd()
+    with current_directory(tmp_dir):
+        temp_dir = os.getcwd()
+    assert temp_dir == tmp_dir
+    assert os.getcwd() == curr_dir
+
+
+def test_add_path():
+    path = tempfile.gettempdir()
+    with add_path(path, prepend = True):
+        prep = sys.path[0]
+    with add_path(path, prepend=False):
+        app = sys.path[-1]
+    assert path == app, prep
+    assert path not in sys.path
