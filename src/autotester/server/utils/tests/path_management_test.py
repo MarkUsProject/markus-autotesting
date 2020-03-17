91d9e16935e4b07e362816a4179d806510f04669
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-03-17 15:35:21 -0400

    test: Added more checks

--- src/autotester/server/utils/tests/path_management_test.py
+++ src/autotester/server/utils/tests/path_management_test.py
@@ -1,15 +1,15 @@
-from src.autotester.server.utils.path_management import current_directory, add_path
+from autotester.server.utils.path_management import current_directory, add_path
 import tempfile
 import os
 import sys
 
 
 def test_current_directory():
-    tmp_dir = tempfile.gettempdir()
+    dir = tempfile.gettempdir()
     curr_dir = os.getcwd()
-    with current_directory(tmp_dir):
+    with current_directory(dir):
         temp_dir = os.getcwd()
-    assert temp_dir == tmp_dir
+    assert temp_dir == dir
     assert os.getcwd() == curr_dir
 
 
@@ -21,3 +21,14 @@ def test_add_path():
         app = sys.path[-1]
     assert path == app, prep
     assert path not in sys.path
+
+    sys_path = tempfile.gettempdir()
+    sys.path.append(sys_path)
+    with add_path(sys_path, prepend=True):
+        prepend = sys.path[0]
+    with add_path(sys_path, prepend=False):
+        append = sys.path[-1]
+    assert sys_path == append, prepend
+    assert sys_path in sys.path
+    sys.path.pop(-1)
+
