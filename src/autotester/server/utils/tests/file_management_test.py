7666a4a518d15a722141335eeeb4669608530ba4
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-03-20 10:11:16 -0400

    test: Added tests

--- src/autotester/server/utils/tests/file_management_test.py
+++ src/autotester/server/utils/tests/file_management_test.py
@@ -1,10 +1,11 @@
-import stat
 from src.autotester.server.utils.file_management import *
 import os.path
 import re
 import tempfile
 import shutil
 from src.autotester.config import config
+import subprocess
+
 FILES_DIRNAME = config["_workspace_contents", "_files_dir"]
 
 
@@ -90,17 +91,16 @@ def test_fd_open():
     os.close(file_fd)
 
 
-def test_fd_lock():
-    des, file = tempfile.mkstemp()
+def test_fd_lock_p1():
+    temp_dir = tempfile.mkdtemp()
+    fd, temp_file = tempfile.mkstemp(dir=temp_dir)
     access = True
-    with fd_open(file) as fd:
+    with fd_open(temp_dir) as fd:
         with fd_lock(fd):
             try:
-                os.close(fd)
-            except IOError as e:
-                print(e)
+                subprocess.run(["rm", {temp_file}])
+            except:
                 access = False
-    print(access)
     assert not access
 
 
@@ -118,56 +118,3 @@ def test_copy_test_script_files():
     assert os.path.exists(copied_file)
     os.close(file_fd)
     shutil.rmtree(tests_path)
-
-
-def test_setup_files():
-    markus_address = "http://localhost:3000/csc108/en/main"
-    assignment_id = 1
-    tests_path = tempfile.mkdtemp()
-    file_fd, test_file = tempfile.mkstemp(dir=tests_path)
-    files_path = tempfile.mkdtemp()
-    f_d, files = tempfile.mkstemp(dir=files_path)
-    student_files, script_files = setup_files(
-        files_path, tests_path, markus_address, assignment_id
-    )
-    st_files = []
-    scr_files = []
-    [st_files.append(i[1]) for i in student_files]
-    [scr_files.append(j[1]) for j in script_files]
-    stdir_pmsn = []
-    stfile_pmsn = []
-    scdir_pmsn = []
-    scfile_pmsn = []
-    for fd, file_or_dir in student_files:
-        if fd == "d":
-            mode = os.stat(file_or_dir).st_mode
-            permission = stat.filemode(mode)
-            stdir_pmsn.append(
-                True
-            ) if permission == "-rwxrwx---" else stdir_pmsn.append(False)
-        else:
-            mode = os.stat(file_or_dir).st_mode
-            permission = stat.filemode(mode)
-            stfile_pmsn.append(
-                True
-            ) if permission == "-rw-rw----" else stfile_pmsn.append(False)
-    for fd, file_or_dir in script_files:
-        if fd == "d":
-            mode = os.stat(file_or_dir).st_mode
-            permission = stat.filemode(mode)
-            scdir_pmsn.append(
-                True
-            ) if permission == "drwxrwx--T" else scdir_pmsn.append(False)
-        else:
-            mode = os.stat(file_or_dir).st_mode
-            permission = stat.filemode(mode)
-            scfile_pmsn.append(
-                True
-            ) if permission == "-rw-r-----" else scfile_pmsn.append(False)
-
-    assert all([os.path.exists(f) for f in st_files])
-    assert all([os.path.exists(f) for f in scr_files])
-    assert all([i for i in stdir_pmsn])
-    assert all([i for i in stfile_pmsn])
-    assert all([i for i in scdir_pmsn])
-    assert all([i for i in scfile_pmsn])
