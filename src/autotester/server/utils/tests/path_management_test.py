e9773981c9006f2fbb00780a67597b48cbc4075d
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-04-13 12:39:54 -0400

    test: Used fixtures

--- src/autotester/server/utils/tests/path_management_test.py
+++ src/autotester/server/utils/tests/path_management_test.py
@@ -2,18 +2,37 @@ from autotester.server.utils.path_management import current_directory, add_path
 import tempfile
 import os
 import sys
+import pytest
+
+@pytest.fixture
+def change_work_dir():
+    temp_dir = tempfile.gettempdir()
+    with current_directory(temp_dir):
+        temp_work_dir = os.getcwd()
+        yield temp_work_dir, temp_dir
 
 
-def test_current_directory():
-    dir = tempfile.gettempdir()
-    curr_dir = os.getcwd()
-    with current_directory(dir):
-        temp_dir = os.getcwd()
-    assert temp_dir == dir
-    assert os.getcwd() == curr_dir
+class TestCurrentDirectory:
+    def test_temp_work_dir(self, change_work_dir):
+        """
+        Checks whether the working directory is changed from current to temporary working directory
+        """
+        temp_work_dir, temp_dir = change_work_dir
+        assert temp_work_dir == temp_dir
+
+    def test_current_work_dir(self, change_work_dir):
+        """
+        Checks whether the working directory is changed from temporary to current working directory
+        """
+        curr_work_dir = os.getcwd()
+        temp_work_dir, temp_dir = change_work_dir
+        assert os.getcwd() == curr_work_dir
 
 
 def test_add_path():
+    """
+    When adding the path which is not exist
+    """
     path = tempfile.gettempdir()
     with add_path(path, prepend=True):
         prep = sys.path[0]
@@ -24,6 +43,9 @@ def test_add_path():
 
 
 def test_add_existing_path():
+    """
+    When adding the path which exists already
+    """
     sys_path = tempfile.gettempdir()
     sys.path.append(sys_path)
     with add_path(sys_path, prepend=True):
