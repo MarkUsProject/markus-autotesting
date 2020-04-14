7adf94c50d6fc80ff06ec1c2f6bc2acdc040471a
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-04-13 22:45:27 -0400

    Added separate tests for each assert statement

--- src/autotester/server/utils/tests/path_management_test.py
+++ src/autotester/server/utils/tests/path_management_test.py
@@ -4,12 +4,23 @@ import os
 import sys
 import pytest
 
+
 @pytest.fixture
 def change_work_dir():
     temp_dir = tempfile.gettempdir()
     with current_directory(temp_dir):
         temp_work_dir = os.getcwd()
-        yield temp_work_dir, temp_dir
+    return temp_work_dir, temp_dir
+
+
+@pytest.fixture
+def add_path_ap():
+    path = tempfile.gettempdir()
+    with add_path(path, prepend=True):
+        prep = sys.path[0]
+    with add_path(path, prepend=False):
+        app = sys.path[-1]
+    return prep, app, path
 
 
 class TestCurrentDirectory:
@@ -29,29 +40,26 @@ class TestCurrentDirectory:
         assert os.getcwd() == curr_work_dir
 
 
-def test_add_path():
-    """
-    When adding the path which is not exist
-    """
-    path = tempfile.gettempdir()
-    with add_path(path, prepend=True):
-        prep = sys.path[0]
-    with add_path(path, prepend=False):
-        app = sys.path[-1]
-    assert path == app == prep
-    assert path not in sys.path
+class TestAddPath:
+    def test_add_new_path(self, add_path_ap):
+        """
+        When adding the path which is not exist
+        """
+        prep, app, path = add_path_ap
+        assert path not in sys.path
 
+    def test_add_existing_path(self, add_path_ap):
+        """
+        When adding the path which exists already
+        """
+        prep, app, sys_path = add_path_ap
+        sys.path.append(sys_path)
+        assert sys_path in sys.path
+        sys.path.pop(-1)
 
-def test_add_existing_path():
-    """
-    When adding the path which exists already
-    """
-    sys_path = tempfile.gettempdir()
-    sys.path.append(sys_path)
-    with add_path(sys_path, prepend=True):
-        prepend = sys.path[0]
-    with add_path(sys_path, prepend=False):
-        append = sys.path[-1]
-    assert sys_path == append == prepend
-    assert sys_path in sys.path
-    sys.path.pop(-1)
+    def test_path_ap(self, add_path_ap):
+        """
+        Checks the path is appended and prepended to sys.path
+        """
+        prep, app, path = add_path_ap
+        assert path == app == prep
