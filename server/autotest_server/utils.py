11d96bf8e4782b7d11276996c56cb8f9de51a440
ch-iv <icheppy101@gmail.com>
2025-02-22 22:47:28 -0500

    Refactor setting resource limits

--- server/autotest_server/utils.py
+++ server/autotest_server/utils.py
@@ -7,8 +7,6 @@ from io import BytesIO
 from typing import Type, Optional, Tuple, List, Generator
 from .config import config
 
-RLIMIT_ADJUSTMENTS = {"nproc": 10}
-
 
 def loads_partial_json(json_string: str, expected_type: Optional[Type] = None) -> Tuple[List, bool]:
     """
@@ -47,29 +45,13 @@ def _rlimit_str2int(rlimit_string):
 
 def set_rlimits_before_test() -> None:
     """
-    Sets rlimit settings specified in config file
-    This function ensures that for specific limits (defined in RLIMIT_ADJUSTMENTS),
-    there are at least n=RLIMIT_ADJUSTMENTS[limit] resources available for cleanup
-    processes that are not available for test processes.  This ensures that cleanup
-    processes will always be able to run.
+    Sets resource limits specified in the config.
     """
-    for limit_str in config.get("rlimit_settings", {}).keys() | RLIMIT_ADJUSTMENTS.keys():
-        limit = _rlimit_str2int(limit_str)
-        config_soft, config_hard = config.get("rlimit_settings", {}).get(limit_str, resource.getrlimit(limit))
-        curr_soft, curr_hard = resource.getrlimit(limit)
-        # account for the fact that resource.RLIM_INFINITY == -1
-        soft, hard = min(curr_soft, config_soft), min(curr_hard, config_hard)
-        if soft < 0:
-            soft = max(curr_soft, config_soft)
-        if hard < 0:
-            hard = max(curr_hard, config_hard)
-        # reduce the hard limit so that cleanup scripts will have at least adj more resources to use.
-        adj = RLIMIT_ADJUSTMENTS.get(limit_str, 0)
-        if hard >= adj:
-            hard -= adj
-        # make sure the soft limit doesn't exceed the hard limit
-        soft = min(hard, soft)
-        resource.setrlimit(limit, (soft, hard))
+    rlimit_settings = config.get("rlimit_settings", {})
+
+    for resource_str, rlimit in rlimit_settings.items():
+        # Raises an exception if the rlimit is invalid or the underlying system call fails.
+        resource.setrlimit(_rlimit_str2int(resource_str), rlimit)
 
 
 def extract_zip_stream(zip_byte_stream: bytes, destination: str) -> None:
