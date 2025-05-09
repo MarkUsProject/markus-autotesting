a7a3ea1d946986d9d4c7126fd7ab281e517d5727
ch-iv <icheppy101@gmail.com>
2025-05-08 22:33:53 -0400

    Use importlib instead of __import__

--- server/autotest_server/run_test_script.py
+++ server/autotest_server/run_test_script.py
@@ -1,5 +1,6 @@
 import sys
 import msgspec
+import importlib
 
 # Different parts of data that we need is sent on stdin separated by newlines
 tester_type = sys.stdin.readline().strip()
@@ -7,15 +8,13 @@ resource_settings = msgspec.json.decode(sys.stdin.readline().strip())
 specs_json = sys.stdin.read()
 
 # Dynamically import the models module based on the tester type
-models_module = __import__("testers.models", fromlist=[f"{tester_type.capitalize()}TestSpecs"])
+models_module = importlib.import_module("testers.models")
 # This imports an msgspec Struct class used to decode the specs
 TestSpecs = getattr(models_module, f"{tester_type.capitalize()}TestSpecs")
 specs = msgspec.json.decode(specs_json, type=TestSpecs)
 
 # Dynamically import the tester module based on the tester type
-tester_module = __import__(
-    f"testers.{tester_type}.{tester_type}_tester", fromlist=[f"{tester_type.capitalize()}Tester"]
-)
+tester_module = importlib.import_module(f"testers.{tester_type}.{tester_type}_tester")
 Tester = getattr(tester_module, f"{tester_type.capitalize()}Tester")
 
 Tester(resource_settings=resource_settings, specs=specs).run()
