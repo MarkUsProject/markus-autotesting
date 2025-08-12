fa0679b6203458cfa7c5cb30ef377392a1c31563
Will Kukkamalla <will.kukkamalla@mail.utoronto.ca>
2025-08-12 15:02:06 -0400

    added throwing errors

--- server/autotest_server/testers/r/setup.py
+++ server/autotest_server/testers/r/setup.py
@@ -1,9 +1,19 @@
 import os
 import json
 import subprocess
+import logging
+
+logging.basicConfig(
+    filename="/app/logs/ai_feedback.log",  # or '/app/logs/ai_feedback.log'
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logger = logging.getLogger(__name__)
+logger.info("Logger initialized and working setup")
 
 
 def create_environment(settings_, env_dir, default_env_dir):
+    logger.info("Creating environment")
     env_data = settings_.get("env_data", {})
     lockfile_submitted = env_data.get("renv.lock", False)
     os.makedirs(env_dir, exist_ok=True)
@@ -18,6 +28,7 @@ def create_environment(settings_, env_dir, default_env_dir):
         text=True,
         capture_output=True,
     )
+    logger.info("installed r_tester_setup")
 
     if lockfile_submitted:
         renv_lock_path = os.path.join(env_dir, "../", "files", "renv.lock")
@@ -30,6 +41,7 @@ def create_environment(settings_, env_dir, default_env_dir):
                 text=True,
                 capture_output=True,
             )
+    logger.info("Created environment")
     return {**env, "PYTHON": os.path.join(default_env_dir, "bin", "python3")}
 
 
@@ -39,4 +51,6 @@ def settings():
 
 
 def install():
+    logger.info("Installing R")
     subprocess.run(os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.system"), check=True)
+    logger.info("DONE installing R")
