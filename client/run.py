da44cef52a259f52682d8dd9d3618e08a073bf7f
Will Kukkamalla <will.kukkamalla@mail.utoronto.ca>
2025-08-14 09:21:53 -0400

    added config to set json objects to not be sorted with sent

--- client/run.py
+++ client/run.py
@@ -1,5 +1,7 @@
 import os
 from autotest_client import app
 
+app.config["JSON_SORT_KEYS"] = False
+
 if __name__ == "__main__":
     app.run(host=os.environ["FLASK_HOST"], port=int(os.environ["FLASK_PORT"]), debug=True)
