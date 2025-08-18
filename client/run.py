08e9d2d9055a75f782d8b5790e3e624b70c1fff7
Will Kukkamalla <will.kukkamalla@mail.utoronto.ca>
2025-08-18 12:01:15 -0400

    removed extra line

--- client/run.py
+++ client/run.py
@@ -1,7 +1,6 @@
 import os
 from autotest_client import app
 
-
 if __name__ == "__main__":
     app.json.sort_keys = False
     app.run(host=os.environ["FLASK_HOST"], port=int(os.environ["FLASK_PORT"]), debug=True)
