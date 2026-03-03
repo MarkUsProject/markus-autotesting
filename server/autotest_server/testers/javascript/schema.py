a72d5158ca67d29a1ebbda2c9e0f0864777ef892
Liz <lizzie@Liz.>
2026-03-02 20:52:10 -0500

    schema to define basic tester structure

---
+++ server/autotest_server/testers/javascript/schema.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from typing import Annotated, Literal
+from msgspec import Meta, Struct
+from markus_autotesting_core.types import BaseTestData, BaseTesterSettings
+
+
+class JsTesterSettings(BaseTesterSettings):
+    """The settings for the JavaScript tester."""
+    test_data: Annotated[list[JsTestData], Meta(title="Test Groups", min_length=1)]
+
+
+class JsTestData(BaseTestData, kw_only=True):
+    """The `test_data` specification for the JavaScript tester."""
+    test_timeout: Annotated[int, Meta(title="Test timeout (ms)", ge=0)] = 5000
