cefa3f3f797585269eeb457c13132f70b2e0ec8b
freyazjiner <jinerz77@gmail.com>
2026-03-20 01:11:30 -0400

    remove the redundant lines

--- server/autotest_server/testers/js/schema.py
+++ server/autotest_server/testers/js/schema.py
@@ -6,11 +6,9 @@ from markus_autotesting_core.types import BaseTestData, BaseTesterSettings
 
 class JsTesterSettings(BaseTesterSettings):
     """The settings for the JavaScript tester."""
-
     test_data: Annotated[list[JsTestData], Meta(title="Test Groups", min_length=1)]
 
 
 class JsTestData(BaseTestData, kw_only=True):
     """The `test_data` specification for the JavaScript tester."""
-
     pass
