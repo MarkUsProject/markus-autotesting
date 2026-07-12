47d52493e4924fcf09cf9f2ca6fa446977a0de33
David Liu <david@cs.toronto.edu>
2026-07-12 19:21:53 -0400

    Added localhost:3000 forwarding to server Docker container (#740)

--- server/.dockerfiles/cmd-dev.sh
+++ server/.dockerfiles/cmd-dev.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Forward localhost:3000 to the rails container, so that MarkUs URLs captured from browser
+# requests (which use localhost) resolve correctly when this container calls back into MarkUs.
+socat TCP-LISTEN:3000,fork,reuseaddr TCP:rails:3000 &
+
 markus_venv/bin/python /app/install.py
 markus_venv/bin/python /app/start_stop.py start -n
